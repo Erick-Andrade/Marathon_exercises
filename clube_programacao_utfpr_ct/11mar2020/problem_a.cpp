c495b1581432fdfbbf275e2a375ce58649d41430
Erick-Andrade <erickprog12@gmail.com>
2023-10-11 23:27:26 -0300

    Exercises 10/11/2023

---
+++ clube_programacao_utfpr_ct/11mar2020/problem_a.cpp
@@ -0,0 +1,23 @@
+// https://www.urionlinejudge.com.br/judge/problems/view/2987
+
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define ll long long
+#define endl '\n'
+#define f first
+#define s second
+#define pb push_back
+#define _ ios_base::sync_with_stdio(0); cin.tie(0);
+ 
+const int MAX = 1e6+10;
+const int IINF = INT_MAX;
+const int INF = 0x3f3f3f3f;
+const ll lINF = 0x3f3f3f3f3f3f3f3fll;
+ 
+int main() { 
+    char c; cin >> c;
+    cout << c - 64 << endl;
+    return 0;
+}
---
+++ spoj/obi/quadrados.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    int n, quadrado;
+    scanf("%d", &n);
+    quadrado = n * n;
+    printf("%d\n", quadrado);
+
+    return 0;
+}
